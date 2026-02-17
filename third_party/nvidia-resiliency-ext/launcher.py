d5ab1e73ef56e3fdcbdf0c3002848961735f5fab
Tarun-SnoDx <maganur@google.com>
2026-02-17 15:20:07 -0800

    Update launcher.py
    attemps|attempts

--- third_party/nvidia-resiliency-ext/launcher.py
+++ third_party/nvidia-resiliency-ext/launcher.py
@@ -1978,7 +1978,7 @@ def get_use_env(args) -> bool:
 
 
 def _get_logs_specs_class(logs_specs_name: Optional[str]) -> Type[LogsSpecs]:
-  """attempts to load `torchrun.logs_spec` entrypoint with key of `logs_specs_name` param.
+  """Attempts to load `torchrun.logs_spec` entrypoint with key of `logs_specs_name` param.
 
   Provides plugin mechanism to provide custom implementation of LogsSpecs.
 
